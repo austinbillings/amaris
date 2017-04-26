2832f88d545b1f4c199db4830039f9fe89822efe
Austin Billings <austinjosephbillings@gmail.com>
2017-04-25 22:16:54 -0400

    v update to 1.1.0

--- src/amaris.js
+++ src/amaris.js
@@ -10,7 +10,7 @@ const collector = require('colleqtor');
 
 let amaris = {
   params,
-  version: '0.0.2',
+  version: '1.1.0',
   blueprints: collector.listFiles(__dirname + '/../blueprints/', null, true),
   // Require Options -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
   require (requirements, input) {
